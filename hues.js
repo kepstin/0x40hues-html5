85ed962c5294c99c2d24c4420c562da3422b37b5
Calvin Walton <calvin.walton@kepstin.ca>
2015-06-14 18:38:33 -0400

    Drop the builtin images autoload.

--- hues.js
+++ hues.js
@@ -1299,10 +1299,6 @@
       var builtin = respacks[0];
       var respack = respacks[1];
 
-      if (builtin["images"]) {
-        addImages("builtin");
-      }
-
       if (respack["hues"]) {
         addHues(respack["name"]);
       } else {
